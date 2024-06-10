26a2f12740378f0c6e6766b5261498b980fdffc1
TestProGio <giovanna.silva@testpro.io>
2024-06-10 14:20:06 -0400

    Class20-Notes

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,42 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
 public class BaseTest {
-    // This is a change
+    WebDriver driver = null;// 1- declared
+    ChromeOptions options = ChromeOptions();
+    WebDriverWait wait; // 2- initialized
+
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
+   @BeforeMethod
+   @Parameter({"BaseURL"})
+   //Precondition
+   //Added ChromeOptions argument below
+   options.addArguments("--remote-allow-origins=*");
+   driver= new ChromeDriver(options);
+   driver.manage().timeouts.implicitWait(Duration.ofSeconds(10));
+   driver.manage().window().maximize();
+   // 3- new line using max 10 seconds wait.
+        // loading web driver with driver and max time of 10 sec.
+   wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+   navigateToPage(baseURL);
+
+    }
+    @AfterMethod
+    public void closeBrowser() { driver.quit()}
+
+    protected void submit() throws InterruptedException {
+        // Using WAIT WITH CONDITIONS
+        WebElement submit = wait.until(ExpectedConditions.visibilityOfElementLocated(By.ByCssSelector("button[type='submit']")));
+        submit.click();
+        //Thread.sleep(1000);
+
     }
 }
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,6 +1,8 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -19,6 +21,26 @@ public class LoginTests extends BaseTest {
 
         String url = "https://qa.koel.app/";
         driver.get(url);
+        Assert.assertEquals(driver.getCurrentUrl(), url);
+        driver.quit();
+        // This is a change
+        // This is another change #2
+    }
+    // new method
+    public void loginValidEmailPassword() {
+        //navigateToPage();
+
+        enterEmail("demo@testpro.io");
+        enterPassword("te$t$tudent");
+        submit();
+//Thread.sleep(2000);
+wait.until(ExpectedConditions.elementToBeClickable());
+//WebElement avatarIcon = driver.findElement(By.ByCssSelector("image[class='avatar']"));
+wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("image[class='avatar']"));
+wait.until(ExpectedConditions.invisibilityOfElementLocated()Assert.assertEquals(driver.getCurrentUrl(), url);
+Assert.assertTrue(avatarIcon.isDisplayed());
+
+//
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
         // This is a change
